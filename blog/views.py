861e4cb37216e4bc9ddb48f7168b97a6f237fcaf
OM VERMA <omv71@gmail.com>
2026-07-09 13:31:22 +0530

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,151 @@
+from django.conf import settings
+print("EMAIL BACKEND:", settings.EMAIL_BACKEND)
+
+# Create your views here.
+from django.shortcuts import get_object_or_404, render
+from .models import Post
+
+from django.core.paginator import EmptyPage, Paginator, PageNotAnInteger
+from django.views.generic import ListView   
+from .forms import CommentForm, EmailPostForm
+from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
+from django.db.models import Count
+from taggit.models import Tag
+
+def post_detail(request,year, month, day, post):
+    post = get_object_or_404(
+        Post,
+        slug=post,
+        status=Post.Status.PUBLISHED,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day)
+
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(
+        tags__in=post_tags_ids
+        ).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(
+            same_tags=Count('tags')
+    ).order_by('-same_tags', '-publish')[:4]        
+    return render(
+        request,
+        'blog/post/detail.html',
+        {'post': post,
+         'comments': comments,
+         'form': form}
+    )
+
+
+def post_list(request,tag_slug=None):
+    post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page',1)
+    try:
+       posts = paginator.get_page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)   
+    except EmptyPage:
+        posts = paginator.get_page(paginator.num_pages)
+
+    return render(
+        request,
+        'blog/post/list.html',
+        {'posts': posts,
+         'tag': tag}
+
+    )
+class PostListView(ListView):
+
+  queryset = Post.published.all()
+  context_object_name = 'posts'
+  paginate_by = 2
+  template_name = 'blog/post/list.html'   
+
+
+def post_share(request, post_id):
+
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+
+    sent = False  
+
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+
+        if form.is_valid():
+            cd = form.cleaned_data
+
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url()
+            )
+
+            subject = (
+                f"{cd['name']} ({cd['email']}) "
+                f"recommends you read {post.title}"
+            )
+
+            message = (
+                f"Read {post.title} at {post_url}\n\n"
+                f"{cd['name']}'s comments: {cd['comments']}"
+            )
+
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=settings.EMAIL_HOST_USER,
+                recipient_list=[cd['to']],
+            )
+
+            sent = True
+
+    else:
+        form = EmailPostForm()
+
+    return render(
+        request,
+        'blog/post/share.html',
+        {
+            'post': post,
+            'form': form,
+            'sent': sent
+        }
+    )
+
+@require_POST
+def post_comment(request, post_id):
+
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+
+    comment = None
+
+    form = CommentForm(data=request.POST)
+
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+
+    return render(
+        request,
+        'blog/post/comment.html',
+        {
+            'post': post,
+            'form': form,
+            'comment': comment
+        }
+    )
